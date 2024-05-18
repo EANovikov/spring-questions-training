a87b0df35bf8d57061c06c76ec5c193e600037f4
Evgeny Novikov <xevgnov@gmail.com>
2024-05-18 07:10:51 +0200

    question4 started

--- 4-autowire-injection-types/autowire/src/main/java/com/xevgnov/autowire/domain/Order.java
+++ 4-autowire-injection-types/autowire/src/main/java/com/xevgnov/autowire/domain/Order.java
@@ -1,5 +1,16 @@
 package com.xevgnov.autowire.service;
 
+import java.util.UUID;
+
+@Builder
+@Getters
+@Setters
 public class Order {
+
+  private UUID id = UUID.randomUUID();  
+  private String clientEmail;  
+  private String deliveryAddress;
+  private String dish;
+  private UUID paymentId;
   
 }

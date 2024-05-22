70db5192e2f2f54506201f6363563f9d3266043a
Evgeny Novikov <xevgnov@gmail.com>
2024-05-22 22:17:49 +0200

    question5 started

--- 5-stereotypes-meta-annotations/stereotypes/src/main/java/com/xevgnov/stereotypes/controller/DemoController.java
+++ 5-stereotypes-meta-annotations/stereotypes/src/main/java/com/xevgnov/stereotypes/controller/DemoController.java
@@ -3,13 +3,15 @@ package com.xevgnov.stereotypes.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
-@RequestMapping("/controller")
+@RequestMapping("/demo")
 public class DemoController {
 
     @GetMapping
-    public String demo(){
-      return "index";
+    public String demo(Model model){
+      model.addAttribute("name", this.getClass().getName());
+      return "demo";
     }
 }

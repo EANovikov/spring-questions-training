c0b81522048b3d8d71be948bc0b7c59d871db9e6
Evgeny Novikov <xevgnov@gmail.com>
2024-07-03 07:37:38 +0200

    question12 started

--- 12-n-plus-one-problem/n-plus-one/src/main/java/com/xevgnov/n.plus.one/service/BookStoreServiceImpl.java
+++ 12-n-plus-one-problem/n-plus-one/src/main/java/com/xevgnov/n.plus.one/service/BookStoreServiceImpl.java
@@ -1,17 +1,24 @@
 package com.xevgnov.n.plus.one.service;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
+import com.xevgnov.n.plus.one.entity.Author;
 import com.xevgnov.n.plus.one.repository.AuthorRepository;
 
 @Service
-public class BookStoreServiceImpl {
+public class BookStoreServiceImpl implements BookStroreService {
 
     private final AuthorRepository authorRepository;
 
     public BookStoreServiceImpl(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
     }
-    
+     
+    @Override
+    public Set<Author> getAuthors(){
+        //todo
+    }
     
 }

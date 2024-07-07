7f3223f2f45b627f714460b5f9ea75ac83bf4ff1
Evgeny Novikov <xevgnov@gmail.com>
2024-07-07 10:58:14 +0200

    question12 continue

--- 12-n-plus-one-problem/n-plus-one/src/main/java/com/xevgnov/n/plus/one/entity/Article.java
+++ 12-n-plus-one-problem/n-plus-one/src/main/java/com/xevgnov/n/plus/one/entity/Article.java
@@ -4,15 +4,19 @@ import java.time.Instant;
 import java.util.UUID;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
+import lombok.Value;
 
-@Data
+@Value
 @Entity
+@Table(name = "ARTICLE")
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -22,6 +26,7 @@ public class Article {
     private Instant created;
     private Instant updated;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "AUTHOR_ID")
     private Author author;
 }
--- 12-n-plus-one-problem/n-plus-one/src/main/java/com/xevgnov/n/plus/one/entity/Author.java
+++ 12-n-plus-one-problem/n-plus-one/src/main/java/com/xevgnov/n/plus/one/entity/Author.java
@@ -3,14 +3,20 @@ package com.xevgnov.n.plus.one.entity;
 import java.util.Set;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Value;
 
 @Entity
+@Table(name = "AUTHOR")
+@Value
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -18,7 +24,9 @@ public class Author {
     private String name;
     private String surname;
 
-    @OneToMany
-    @JoinColumn(name = "book_id")
+    @OneToMany(fetch = FetchType.LAZY,
+        //mappedBy = "AUTHOR",
+    cascade = CascadeType.ALL)
+   // @JoinColumn(name = "book_id")
     private Set<Article> books;
 }

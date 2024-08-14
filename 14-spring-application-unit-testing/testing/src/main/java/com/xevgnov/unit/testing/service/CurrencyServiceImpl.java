f36c0d055e6fcb42a56d4c20424bd2dc6c7a110d
Evgeny Novikov <xevgnov@gmail.com>
2024-08-14 07:54:11 +0200

    question 13/14 save progress

--- 14-spring-application-unit-testing/testing/src/main/java/com/xevgnov/unit/testing/service/CurrencyServiceImpl.java
+++ 14-spring-application-unit-testing/testing/src/main/java/com/xevgnov/unit/testing/service/CurrencyServiceImpl.java
@@ -3,7 +3,11 @@ package com.xevgnov.unit.testing.service;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalDouble;
 
 import org.springframework.stereotype.Service;
 
@@ -23,6 +27,7 @@ public class CurrencyServiceImpl implements CurrencyService {
     private final FxRatesApiClient fxRatesApiClient;
     private final ExchangeRecordsRepository exchangeRecordsRepository;
     private final ObjectMapper mapper = new ObjectMapper();
+    private final  DateTimeFormatter datePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public CurrencyServiceImpl(FxRatesApiClient fxRatesApiClient, 
                                ExchangeRecordsRepository exchangeRecordsRepository) {
@@ -32,25 +37,45 @@ public class CurrencyServiceImpl implements CurrencyService {
 
     public ExchangeStatistics getStatistics(String currencySell, String currencyBuy) {
         List<FxRatesResponse> responses = fetchFxRatesPerWeek(currencySell, currencyBuy);
-        ExchangeStatisticsBuilder statistics = ExchangeStatistics.builder();
-        //todo add stats
-        return statistics.build();
+        return ExchangeStatistics.builder()
+        .buyCurrency(currencyBuy)
+        .sellCurrency(currencySell)
+        .date(responses.get(0).getDate().format(datePattern))
+        .currentPrice(responses.get(0).getRates().get(currencyBuy))
+        .middlePrice(getMiddlePrice(responses, currencyBuy))
+        .priceHistory(getPriceHistory(responses, currencyBuy))
+        .build();
+    }
+
+    private Map<String, String> getPriceHistory(List<FxRatesResponse> responses, String currency){
+        Map<String, String> priceHistory = new LinkedHashMap<>();
+        for(FxRatesResponse response : responses){
+           String date = response.getDate().format(datePattern);
+           String rate = String.valueOf(response.getRates().get(currency));
+           priceHistory.put(date, rate);
+        }
+        return priceHistory;
+    }
+
+    private Double getMiddlePrice(List<FxRatesResponse> responses, String currency){
+        return responses.stream()
+        .mapToDouble(response -> response.getRates().get(currency))
+        .average()
+        .orElse(0);
     }
 
     private List<FxRatesResponse> fetchFxRatesPerWeek(String currencySell, String currencyBuy){
         List<FxRatesResponse> responses = new ArrayList<>();
-        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate today = LocalDate.now();
 
         for(int i = 0; i < 7; i++){
             LocalDate currentDate = today.minusDays(i);
-            String formattedDate = currentDate.format(pattern);
+            String formattedDate = currentDate.format(datePattern);
 
             FxRatesResponse result = fxRatesApiClient.getConvertationRate(formattedDate, currencyBuy, currencySell);
             responses.add(result);
-     
         }
-        
+        return responses;
     }
 
 }

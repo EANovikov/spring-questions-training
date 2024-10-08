17c73695609bfc1305cb6fa8e03c46b4130d6d23
Evgeny Novikov <xevgnov@gmail.com>
2024-10-08 07:06:17 +0200

    question 14 - done mock mvc testing

--- 14-integration-testing-with-spring/spring-testing/src/test/java/com/xevgnov/spring/testing/controller/ExchangeControllerTest.java
+++ 14-integration-testing-with-spring/spring-testing/src/test/java/com/xevgnov/spring/testing/controller/ExchangeControllerTest.java
@@ -1,8 +1,8 @@
 package com.xevgnov.spring.testing.controller;
 
-import static org.hamcrest.Matchers.contains;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
@@ -10,11 +10,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-
 import org.hamcrest.core.StringContains;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -24,6 +20,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.xevgnov.spring.testing.dto.ExchangeStatistics;
+import com.xevgnov.spring.testing.exception.StatisticsServiceException;
 import com.xevgnov.spring.testing.service.StatisticsService;
 
 @WebMvcTest(ExchangeController.class)
@@ -38,7 +35,7 @@ public class ExchangeControllerTest {
     private final DateTimeFormatter datePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Test
-    void testGetStatisticsReturn200Status() throws Exception {
+    void testGetStatisticsReturns200Status() throws Exception {
         // Given
         String sellCurrency = "USD";
         String buyCurrency = "EUR";
@@ -69,7 +66,7 @@ public class ExchangeControllerTest {
 
     @ParameterizedTest
     @ValueSource(strings = { "LONG", "SH", " ", "123", "@#$" })
-    void testGetStatisticsReturn400ExceptionResponseIfSellCurrencyIsNot3UpperCasedCharacters(String sellCurrency)
+    void testGetStatisticsReturns400ExceptionResponseIfSellCurrencyIsNot3UpperCasedCharacters(String sellCurrency)
             throws Exception {
         // Given
         String expectedMessage = "Currency code must contain 3 upper-cased caracters";
@@ -85,7 +82,8 @@ public class ExchangeControllerTest {
 
     @ParameterizedTest
     @ValueSource(strings = { "LONG", "SH", " ", "123", "@#$" })
-    void testGetStatisticsReturn400ExceptionResponseIfBuyCurrencyIsNot3UpperCasedCharacters(String buyCurrency) throws Exception {
+    void testGetStatisticsReturns400ExceptionResponseIfBuyCurrencyIsNot3UpperCasedCharacters(String buyCurrency)
+            throws Exception {
 
         // Given
         String expectedMessage = "Currency code must contain 3 upper-cased caracters";
@@ -97,6 +95,22 @@ public class ExchangeControllerTest {
                         .value(StringContains.containsString(expectedMessage)))
                 .andExpect(jsonPath("$.details")
                         .value(StringContains.containsString(expectedException)));
+    }
+
+    @Test
+    void testGetStatisticsReturns500ExceptionResponseIfStatisticsServiceThrownException() throws Exception {
+        // Given
+        String sellCurrency = "EUR";
+        String buyCurrency = "USD";
+        Exception exception = new StatisticsServiceException("test issue", new RuntimeException());
+        when(statisticsService.getStatistics(sellCurrency, buyCurrency)).thenThrow(exception);
+        // When Then
+        mockMvc.perform(get("/{sellCurrency}/to/{buyCurrency}", sellCurrency, buyCurrency))
+                .andExpect(status().is5xxServerError())
+                .andExpect(jsonPath("$.message")
+                        .value(StringContains.containsString(exception.getMessage())))
+                .andExpect(jsonPath("$.details")
+                        .value(StringContains.containsString(exception.toString())));
     }
 
     private Map<String, Double> getPriceHistory() {

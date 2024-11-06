472c035b2e874798b9604295f540fbde31bc6340
Novikov <ENovikov@luxoft.com>
2024-11-06 16:44:52 +0100

    question5 -fix issues

--- 5-stereotypes-meta-annotations/stereotypes/src/main/java/com/xevgnov/stereotypes/annotaton/InfoRestController.java
+++ 5-stereotypes-meta-annotations/stereotypes/src/main/java/com/xevgnov/stereotypes/annotaton/InfoRestController.java
@@ -1,11 +1,23 @@
 package com.xevgnov.stereotypes.annotaton;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
 
+/*
+ * Custom meta-annotation
+ * to get info endpoint for free, use spring-boot-starter-actuator
+ * This is just for demo to show the usage of existing Stereotype annotations & meta-annotations
+ */
 @RestController
-@RequestMapping(path="/app-info", produces = MediaType.TEXT_PLAIN_VALUE)
+@RequestMapping(path = "/api/info", produces = MediaType.TEXT_PLAIN_VALUE)
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
 public @interface InfoRestController {
-    
+
 }

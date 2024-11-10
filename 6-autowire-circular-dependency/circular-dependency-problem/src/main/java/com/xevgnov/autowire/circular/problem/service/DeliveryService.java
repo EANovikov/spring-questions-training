4726afa46257a98962b593fa15ae8e99a201bacb
Novikov <ENovikov@luxoft.com>
2024-11-10 15:14:29 +0100

    question 6 progress save

--- 6-autowire-circular-dependency/circular-dependency-problem/src/main/java/com/xevgnov/autowire/circular/problem/service/DeliveryService.java
+++ 6-autowire-circular-dependency/circular-dependency-problem/src/main/java/com/xevgnov/autowire/circular/problem/service/DeliveryService.java
@@ -2,8 +2,6 @@ package com.xevgnov.autowire.circular.problem.service;
 
 import java.util.UUID;
 
-import com.xevgnov.autowire.circular.problem.dto.Order;
-
 public interface DeliveryService {
     
     void deliveryOrder(UUID id);

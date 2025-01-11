1299529b07fd1590240c100e4e957ccd3e4c2292
Evgeny Novikov <xevgnov@gmail.com>
2025-01-11 16:09:08 +0100

    question 16 aditionall example -wiremock and H2

--- 16-integration-testing-with-spring/spring-testing/src/test/java/com/xevgnov/spring/testing/integration/ExchangeControllerWireMockH2Test.java
+++ 16-integration-testing-with-spring/spring-testing/src/test/java/com/xevgnov/spring/testing/integration/ExchangeControllerWireMockH2Test.java
@@ -1,5 +1,37 @@
 package com.xevgnov.spring.testing.integration;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor
+import static com.xevgnov.spring.testing.util.TestDataUtility.DATE_PATTERN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.wiremock.spring.EnableWireMock;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.xevgnov.spring.testing.client.FxRatesApiClient;
+import com.xevgnov.spring.testing.dto.ExchangeStatistics;
+import com.xevgnov.spring.testing.dto.FxRatesResponse;
+
+import feign.Feign;
+import feign.Logger;
+import feign.gson.GsonDecoder;
+import feign.gson.GsonEncoder;
+import feign.okhttp.OkHttpClient;
+import feign.slf4j.Slf4jLogger;
+
 @SpringBootTest
 @EnableWireMock
 public class ExchangeControllerWireMockH2Test {
@@ -9,30 +41,39 @@ public class ExchangeControllerWireMockH2Test {
     @Autowired
     private TestRestTemplate testRestTemplate;
 
+    private FxRatesApiClient fxRatesApiClient = Feign.builder()
+            .client(new OkHttpClient())
+            .encoder(new GsonEncoder())
+            .decoder(new GsonDecoder())
+            .logger(new Slf4jLogger(FxRatesApiClient.class))
+            .logLevel(Logger.Level.FULL)
+            .target(FxRatesApiClient.class, wireMockUrl);
+
     @Autowired
-    FxRatesApiClient fxRatesApiClient;
+    private ObjectMapper objectMapper;
 
     @Test
     void testGetStatisticsReturns200Status() {
         // Given
-        fxRatesApiClient.setBaseUrl(wireMockUrl);
-        
         String sellCurrency = "EUR";
         String buyCurrency = "USD";
-        String date = LocalDate.now().format(DATE_PATTERN);
+        String date = LocalDate.of(2025, 1, 12).format(DATE_PATTERN);
         FxRatesResponse fxRatesResponse = FxRatesResponse.builder()
-                    .base(sellCurrency)
-                    .privacy("privacy")
-                    .terms("terms")
-                    .success(true)
-                    .timestamp(Instant.now())
-                    .date(ZonedDateTime.now())
-                    .rates(Map.of(buyCurrency, rate))
-                    .build();
+                .base(sellCurrency)
+                .privacy("privacy")
+                .terms("terms")
+                .success(true)
+                .timestamp(Instant.now())
+                .date(ZonedDateTime.now())
+                .rates(Map.of(buyCurrency, 1.0271234))
+                .build();
+        var mockedResponse = WireMock.aResponse()
+                .withBody(objectMapper.writeValueAsString(fxRatesResponse))
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", "application/json");
         stubFor(
-            get("/historical?date={date}2&currencies={buyCurrency}&base={sellCurrency}", date, buyCurrency, sellCurrency)
-            .willReturn(fxRatesResponse));
-
+                get(String.format("/historical?date=%s&currencies=%s&base=%s", date, buyCurrency, sellCurrency))
+                        .willReturn(mockedResponse));
 
         // When
         ResponseEntity<ExchangeStatistics> response = testRestTemplate.getForEntity("/{sellCurrency}/to/{buyCurrency}",
@@ -41,6 +82,5 @@ public class ExchangeControllerWireMockH2Test {
         assertThat(response).isNotNull();
         assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
     }
-
 
 }
--- 16-integration-testing-with-spring/spring-testing/src/test/resources/data.sql
+++ 16-integration-testing-with-spring/spring-testing/src/test/resources/data.sql
@@ -1,6 +1,129 @@
 INSERT INTO ECXCANGE_RECORD (DATE, SELL_CURRENCY, BUY_CURRENCY, FX_RATES_RESPONSE)
 VALUES (
-    '2024-10-11', 
+    '2025-01-11', 
     'USD', 
     'EUR', 
-    '{}');
+    '{
+    "success": true,
+    "terms": "https://fxratesapi.com/legal/terms-conditions",
+    "privacy": "https://fxratesapi.com/legal/privacy-policy",
+    "timestamp": 1736553600,
+    "date": "2025-01-11T00:00:00.000Z",
+    "base": "EUR",
+    "rates": {
+        "USD": 1.025083665
+    }
+}');
+
+INSERT INTO ECXCANGE_RECORD (DATE, SELL_CURRENCY, BUY_CURRENCY, FX_RATES_RESPONSE)
+VALUES (
+    '{
+    "success": true,
+    "terms": "https://fxratesapi.com/legal/terms-conditions",
+    "privacy": "https://fxratesapi.com/legal/privacy-policy",
+    "timestamp": 1736467200,
+    "date": "2025-01-10T00:00:00.000Z",
+    "base": "EUR",
+    "rates": {
+        "USD": 1.030003905
+    }
+}');
+
+INSERT INTO ECXCANGE_RECORD (DATE, SELL_CURRENCY, BUY_CURRENCY, FX_RATES_RESPONSE)
+VALUES (
+    '{
+    "success": true,
+    "terms": "https://fxratesapi.com/legal/terms-conditions",
+    "privacy": "https://fxratesapi.com/legal/privacy-policy",
+    "timestamp": 1736380800,
+    "date": "2025-01-09T00:00:00.000Z",
+    "base": "EUR",
+    "rates": {
+        "USD": 1.031672155
+    }
+}');
+
+INSERT INTO ECXCANGE_RECORD (DATE, SELL_CURRENCY, BUY_CURRENCY, FX_RATES_RESPONSE)
+VALUES (
+    '{
+    "success": true,
+    "terms": "https://fxratesapi.com/legal/terms-conditions",
+    "privacy": "https://fxratesapi.com/legal/privacy-policy",
+    "timestamp": 1736294400,
+    "date": "2025-01-08T00:00:00.000Z",
+    "base": "EUR",
+    "rates": {
+        "USD": 1.034350654
+    }
+}');
+
+INSERT INTO ECXCANGE_RECORD (DATE, SELL_CURRENCY, BUY_CURRENCY, FX_RATES_RESPONSE)
+VALUES (
+    '{
+    "success": true,
+    "terms": "https://fxratesapi.com/legal/terms-conditions",
+    "privacy": "https://fxratesapi.com/legal/privacy-policy",
+    "timestamp": 1736208300,
+    "date": "2025-01-07T00:05:00.000Z",
+    "base": "EUR",
+    "rates": {
+        "USD": 1.038248979
+    }
+}');
+
+INSERT INTO ECXCANGE_RECORD (DATE, SELL_CURRENCY, BUY_CURRENCY, FX_RATES_RESPONSE)
+VALUES (
+    '{
+    "success": true,
+    "terms": "https://fxratesapi.com/legal/terms-conditions",
+    "privacy": "https://fxratesapi.com/legal/privacy-policy",
+    "timestamp": 1736121660,
+    "date": "2025-01-06T00:01:00.000Z",
+    "base": "EUR",
+    "rates": {
+        "USD": 1.030651418
+    }
+}');
+
+INSERT INTO ECXCANGE_RECORD (DATE, SELL_CURRENCY, BUY_CURRENCY, FX_RATES_RESPONSE)
+VALUES (
+    '{
+    "success": true,
+    "terms": "https://fxratesapi.com/legal/terms-conditions",
+    "privacy": "https://fxratesapi.com/legal/privacy-policy",
+    "timestamp": 1736035200,
+    "date": "2025-01-05T00:00:00.000Z",
+    "base": "EUR",
+    "rates": {
+        "USD": 1.031204085
+    }
+}');
+
+INSERT INTO ECXCANGE_RECORD (DATE, SELL_CURRENCY, BUY_CURRENCY, FX_RATES_RESPONSE)
+VALUES (
+    '{
+    "success": true,
+    "terms": "https://fxratesapi.com/legal/terms-conditions",
+    "privacy": "https://fxratesapi.com/legal/privacy-policy",
+    "timestamp": 1735948800,
+    "date": "2025-01-04T00:00:00.000Z",
+    "base": "EUR",
+    "rates": {
+        "USD": 1.031204079
+    }
+}');
+
+INSERT INTO ECXCANGE_RECORD (DATE, SELL_CURRENCY, BUY_CURRENCY, FX_RATES_RESPONSE)
+VALUES (
+    '{
+    "success": true,
+    "terms": "https://fxratesapi.com/legal/terms-conditions",
+    "privacy": "https://fxratesapi.com/legal/privacy-policy",
+    "timestamp": 1735862400,
+    "date": "2025-01-03T00:00:00.000Z",
+    "base": "EUR",
+    "rates": {
+        "USD": 1.026936364
+    }
+}');
+

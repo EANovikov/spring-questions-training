2bf2625a6b82d74bcba13c657fbef43e0ffbd437
Evgeny Novikov <xevgnov@gmail.com>
2025-01-17 07:21:55 +0100

    question5 cleanup

--- 5-autowire-not-required/not-required-injection/src/main/java/com/xevgnov/autowire/optional/Application.java
+++ 5-autowire-not-required/not-required-injection/src/main/java/com/xevgnov/autowire/optional/Application.java
@@ -26,33 +26,16 @@ public class Application {
 	/*
 	 * We are trying to inject Analytic service which is not yet implemented
 	 * Spring application fails to start context due to:
-	 * org.springframework.beans.factory.UnsatisfiedDependencyException: Error
-	 * creating bean with name 'commandLineRunner' defined in
-	 * com.xevgnov.autowire.Application: Unsatisfied dependency expressed through
-	 * method 'commandLineRunner' parameter 1: No qualifying bean of type
-	 * 'com.xevgnov.autowire.service.AnalyticServis' available: expected at least 1
-	 * bean which qualifies as autowire candidate.
+	 * org.springframework.beans.factory.NoSuchBeanDefinitionException:
+	 * No qualifying bean of type
+	 * 'com.xevgnov.autowire.optional.service.AnalyticServis' available:
+	 * expected at least 1 bean which qualifies as autowire candidate.
+	 * Uncomment to see the failure.
 	 */
 
-	@Bean
-	CommandLineRunner commandLineRunner(RestaurantService restaurantService,
-			AnalyticServis analyticServis) {
-		restaurantService.makeOrder(Order.builder()
-				.clientEmail("john.doe@gmail.com")
-				.deliveryAddress("Wroclaw, Plac Konstytucji 3 Maja")
-				.dishes(List.of("Pizza", "Chips", "Gyros"))
-				.paymentId(UUID.randomUUID()).build());
-		if (analyticServis != null) {
-			analyticServis.doAnalytics();
-		}
-		return args -> log.info("Done!");
-	}
-
-	// 1) use Autowired with required = false
 	// @Bean
-	// CommandLineRunner commandLineRunner(@Autowired RestaurantService
-	// restaurantService,
-	//  @Autowired(required = false) AnalyticServis analyticServis) {
+	// CommandLineRunner commandLineRunner(RestaurantService restaurantService,
+	// AnalyticServis analyticServis) {
 	// restaurantService.makeOrder(Order.builder()
 	// .clientEmail("john.doe@gmail.com")
 	// .deliveryAddress("Wroclaw, Plac Konstytucji 3 Maja")
@@ -63,6 +46,21 @@ public class Application {
 	// }
 	// return args -> log.info("Done!");
 	// }
+
+	// 1) use Autowired with required = false
+	@Bean
+	CommandLineRunner commandLineRunner(@Autowired RestaurantService restaurantService,
+			@Autowired(required = false) AnalyticServis analyticServis) {
+		restaurantService.makeOrder(Order.builder()
+				.clientEmail("john.doe@gmail.com")
+				.deliveryAddress("Wroclaw, Plac Konstytucji 3 Maja")
+				.dishes(List.of("Pizza", "Chips", "Gyros"))
+				.paymentId(UUID.randomUUID()).build());
+		if (analyticServis != null) {
+			analyticServis.doAnalytics();
+		}
+		return args -> log.info("Done!");
+	}
 
 	// 2) use optional
 	// @Bean

1fd7cf3bb50611fb1baa1efd94000b424d111e49
Evgeny Novikov <xevgnov@gmail.com>
2025-01-19 18:40:37 +0100

    question 0 - DI examples

--- 0-understanding-spring/spring-di/src/main/java/com/xevgnov/spring/di/Application.java
+++ 0-understanding-spring/spring-di/src/main/java/com/xevgnov/spring/di/Application.java
@@ -3,7 +3,6 @@ package com.xevgnov.spring.di;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.xevgnov.spring.di.service.DateTimeService;
-import com.xevgnov.spring.di.service.DateTimeServiceImpl;
 
 public class Application {
     public static void main(String[] args) {

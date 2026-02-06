326734563787b6a6a5e765da37512bcac5a10808
Evgeny Novikov <xevgnov@gmail.com>
2026-02-06 22:44:32 +0100

    updates

--- 04-context-types/spring-web-context-types/src/main/java/com/xevgnov/JavaConfigWebApplicationInitializer.java
+++ 04-context-types/spring-web-context-types/src/main/java/com/xevgnov/JavaConfigWebApplicationInitializer.java
@@ -1,35 +1,39 @@
 package com.xevgnov;
 
+import com.xevgnov.service.DateTimeService;
+import com.xevgnov.service.DateTimeServiceImpl;
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ConfigurableWebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import jakarta.servlet.ServletContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 public class JavaConfigWebApplicationInitializer implements WebApplicationInitializer {
 
-    //1) use AnnotationConfigWebApplicationContext
+    //    public void onStartup(ServletContext container) {
+    //        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+    //        context.registerShutdownHook();
+    //        context.register(ApplicationConfig.class);
+    //        context.setServletContext(container);
+    //        ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(context));
+    //        servlet.setLoadOnStartup(1);
+    //        servlet.addMapping("/");
+    //        context.refresh();
+    //        DateTimeService dateTimeService = context.getBean(DateTimeServiceImpl.class);
+    //        System.out.println("Printing date/time: " + dateTimeService.getDateTime());
+    //    }
+
     public void onStartup(ServletContext container) {
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        XmlWebApplicationContext context = new XmlWebApplicationContext();
         context.registerShutdownHook();
-        context.register(ApplicationConfig.class);
+        context.setConfigLocation("/WEB-INF/application-config.xml");
         context.setServletContext(container);
         ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(context));
         servlet.setLoadOnStartup(1);
         servlet.addMapping("/");
+        context.refresh();
+        DateTimeService dateTimeService = context.getBean(DateTimeServiceImpl.class);
+        System.out.println("Printing date/time: " + dateTimeService.getDateTime());
     }
-
-    //2) use AnnotationConfigWebApplicationContext
-//public void onStartup(ServletContext container) {
-//
-//    XmlWebApplicationContext context = new XmlWebApplicationContext();
-//    context.registerShutdownHook();
-//    context.setConfigLocation("/WEB-INF/application-config.xml");
-//    context.setServletContext(container);
-//    ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(context));
-//    servlet.setLoadOnStartup(1);
-//    servlet.addMapping("/");
-//}
 }
